3e1d3eaef62f64b5b99a6d057bb1e7cd2a753294
Peter Schmitz <petrischmitz@gmail.com>
2014-07-13 19:35:58 +0200

    Initial commit!

---
+++ src/main/scala/de/petomat/myrx/Playground.scala
@@ -0,0 +1,11 @@
+package de.petomat.myrx
+
+object Playground extends App {
+
+  //TODO  modularisiere util damit nicht xml und parser hier als deps
+
+  println("=" * 50)
+  println("*" * 50)
+  println("=" * 50)
+  
+}
