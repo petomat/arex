c658b556887fc108c082f5c370acb651ac178072
Peter Schmitz <petrischmitz@gmail.com>
2014-07-20 00:22:47 +0200

    Introduced DynamicVariable as a replacement for ThreadLocal

--- src/main/scala/de/petomat/myrx/Playground.scala
+++ src/main/scala/de/petomat/myrx/Playground.scala
@@ -1,26 +1,24 @@
 package de.petomat.myrx
-import scala.collection.breakOut
+import scala.collection.IterableView
+import scala.util.DynamicVariable
+import scala.ref.WeakReference
 import de.petomat.util._
 import de.petomat.util.collection._
-import scala.ref.WeakReference
-import scala.collection.IterableView
+
 //TODO  modularisiere util damit nicht xml und parser hier als deps
 
 object Playground extends App {
 
-  println("=" * 100)
-  println("*" * 100)
-  println("=" * 100)
-
-  object Global {
-    import Rx.Types._
-    // TODO ThreadLocal!!!
-    private[Playground] final val rxCallStack = new ThreadLocal[Seq[RX -> SortedSet[RX]]] { override def initialValue = Seq() } // a stack for holding pairs of the current evaluating Rx and its dependencies 
-  }
+  val x = 40
+  println("=" * x)
+  println("*" * x)
+  println("=" * x)
 
   abstract class Rx[T](val name: String) {
     import Rx.Types._
-    @inline protected final def id = hashCode
+    @inline protected final def id: ID = hashCode
+    def idStr = Integer.toString(id, 36)
+    //    println(s"$name.id = $idStr")
     protected final var dependentsPerID: ID |=> WeakReference[RX] = emptySortedMap
     protected final def dependents: Iterable[RX] = {
       dependentsPerID = dependentsPerID.filter { case id -> weakRef => weakRef.get.isDefined } // purge outdated weak references
@@ -30,21 +28,22 @@ object Playground extends App {
     protected final var value = initial
     final def now: T = value
     final def apply(): T = {
-      Global.rxCallStack.set(Global.rxCallStack.get match {
-        case Nil => Nil
-        case init :+ (parent -> siblings) =>
-          // establish dependent
-          if (!dependentsPerID.contains(parent.id)) {
-            println(s"establish dependency: ${name} ~~~> ${parent.name}")
-            dependentsPerID += parent.id -> WeakReference(parent)
-          }
-          // register sibling
-          init :+ (parent -> (siblings + this))
-      })
+      Rx.Global.currentRxAndDeps.value = {
+        Rx.Global.currentRxAndDeps.value map {
+          case rx -> dependencies =>
+            // establish dependent
+            if (!dependentsPerID.contains(rx.id)) {
+              println(s"establish dependency: ${name} ~~~> ${rx.name}")
+              dependentsPerID += rx.id -> WeakReference(rx)
+            }
+            // register sibling
+            rx -> (dependencies + this)
+        }
+      }
       value
     }
     final override def toString = s"$name[${lastDependencies.map(_.name).mkStr}|${dependents.map(_.name).mkStr}]"
-    // to implement:
+    // to implement in subclass:
     protected def initial: T
     private[Playground] def refresh: Unit
   }
@@ -58,6 +57,9 @@ object Playground extends App {
       type RXsPerLevel = Level |=> SortedSet[RX]
     }
     import Types._
+    object Global {
+      private[Rx] final val currentRxAndDeps = new DynamicVariable[Option[RX -> SortedSet[RX]]](None) // the current evaluating Rx and its (accumulated(while current Rx is evaluated)) dependencies 
+    }
     implicit val rxOrd: Ordering[RX] = Ordering by (_.name) // implicit def rxOrd[X]: Ordering[Rx[X]] = Ordering.by(_.name)
     private[Playground] def rxsPerLevel(rxs: Iterable[RX]): RXsPerLevel = { // println(Map("A" -> 1, "B" -> 2) |^+^| Map("B" -> 1, "C" -> 3)) // Map(A -> 1, B -> 2, C -> 3)
       def levelMapForRx(lvl: Level)(rx: RX): LevelPerRX = Map(rx -> lvl) ++ (rx.dependents map levelMapForRx(lvl + 1) reduceOption (_ |^+^| _) getOrElse Map.empty)
@@ -66,20 +68,21 @@ object Playground extends App {
     }
     def apply[T](name: String)(calc: => T): Rx[T] = {
       new Rx[T](name) {
-        private def transact[X](f: => X): X = {
-          Global.rxCallStack.set(Global.rxCallStack.get :+ this -> emptySortedSet(Rx.rxOrd)) // memorize this as parent and no siblings yet for call to calc
-          val x = f
-          val dependenciesOfThis = Global.rxCallStack.get.last._2
-          println(s"dependencies of $name = " + dependenciesOfThis.map(_.name).mkStr)
-          val removedDependencies = lastDependencies -- dependenciesOfThis
+        private def calcValue: T = {
+          val (value, dependenciesOfThis) = {
+            Rx.Global.currentRxAndDeps.withValue(Some(this -> emptySortedSet(Rx.rxOrd))) { // memorize this as parent and no siblings yet for call to calc
+              (calc: T, Rx.Global.currentRxAndDeps.value.get._2: SortedSet[RX])
+            }
+          }
+          val dependencyIDsOfThis: Set[ID] = dependenciesOfThis.toSet[RX] map (_.id) // no sorted set needed, which is perhaps faster than building a sortedset
+          val removedDependencies = lastDependencies filterNot (dependencyIDsOfThis contains _.id) // lastDependencies -- dependenciesOfThis does not work!  
           for (dep <- removedDependencies) { println(s"remove dependency ${dep.name}"); require(dep.dependentsPerID contains this.id); dep.dependentsPerID -= this.id } // TODO performance: remove require
           //for (dep <- removedDependencies) dep.dependentsPerID -= this.id
           lastDependencies = dependenciesOfThis
-          Global.rxCallStack.set(Global.rxCallStack.get.init)
-          x
+          value
         }
-        final def initial: T = transact { calc }
-        final def refresh: Unit = { print(s"refreshing $name : "); transact { value = calc } } // private[Playground] def refresh: Unit = transact { value = calc }
+        final def initial: T = calcValue
+        final def refresh: Unit = { println(s"refreshing $name : "); value = calcValue } // private[Playground] def refresh: Unit = value = calcValue
       }
     }
   }
@@ -96,49 +99,71 @@ object Playground extends App {
   }
 
   object Var {
-    def apply[T](name: String = "noname")(initial: T): Var[T] = new Var(name, initial)
+    object Cookie
+    def apply[T](name: String, cookie: Cookie.type = Cookie)(initial: T): Var[T] = new Var(name, initial)
+    def apply[T](initial: T): Var[T] = new Var(name = "noname", initial)
   }
 
+  // -------------------------------------------
+
+  //  locally {
+  //    val nr = Var(name = "nr")(3)
+  //    val rxs = Rx(name = "rxs") { Seq.tabulate(nr())(nr => Var(name = "vr" + (nr + 1))(nr + 1)) }
+  //    val sum = Rx(name = "sum") {
+  //      rxs().foldLeft(0)(_ + _())
+  //    }
+  //    println(sum.now)
+  //    nr := 5
+  //    println(sum.now)
+  //    nr := 2
+  //    println(sum.now)
+  //  }
+
   locally {
-    var vr1 = Var("vr1")(3)
-    var vr2 = Var("vr2")(true)
-    var vr3 = Var("vr3")(5)
-    var rx1 = Rx("rx1") { if (vr2()) vr1() else vr3() }
-    var rx2 = Rx("rx2") { rx1() * 2 }
-    var rx3 = Rx("rx3") { rx1() * 3 }
-    var rx4 = Rx("rx4") { rx1() * 10 }
-    var rx5 = Rx("rx5") { rx3() + 1 }
-    var rx6 = Rx("rx6") { rx5() + 1 }
-    var rx7 = Rx("rx7") { rx2() + rx4() + rx6() }
-
-    def now(rx: Rx[_]): String = Option(rx) map (_.now.toString) getOrElse "N/A"
-    def printAll = println(Seq(now(vr1), now(vr2), now(vr3), "|", now(rx1), "|", now(rx2), now(rx3), now(rx4), "|", now(rx5), "|", now(rx6), "|", now(rx7)).mkStr)
-
-    def showGraph(vars: Rx.Types.RX*) = for ((level, rxs) <- Rx.rxsPerLevel(vars)) println(level + " : " + rxs.map(_.name).mkStr)
-
-    showGraph(vr1, vr2, vr3)
-
-    printAll
-    vr2 := true
-    printAll
-    vr1 := 1000
-    printAll
-    locally {
-      rx7 = null
-      rx2 = null
-      System.gc
-      printAll
+    val seq = Seq.tabulate(9)(nr => Var(name = "vr" + (nr + 1))(nr + 1))
+    val nr = Var(name = "nr")(3)
+    val rxs = Rx("rxs") { seq take nr() }
+    val sum = Rx("sum") {
+      rxs().foldLeft(0)(_ + _())
     }
-    vr1 := 10000
-    printAll
+    println(sum.now)
+    nr := 5
+    println(sum.now)
+    nr := 2
+    println(sum.now)
   }
 
   //  locally {
-  //    val vr = Var("vr")(8)
-  //    val rx = Rx("rx") {
-  //      val innerRx = Rx("innerRx") { vr() + 1 }
-  //      3
+  //    var vr1 = Var(name = "vr1")(3)
+  //    var vr2 = Var(name = "vr2")(true)
+  //    var vr3 = Var(name = "vr3")(5)
+  //    var rx1 = Rx(name = "rx1") { if (vr2()) vr1() else vr3() }
+  //    var rx2 = Rx(name = "rx2") { rx1() * 2 }
+  //    var rx3 = Rx(name = "rx3") { rx1() * 3 }
+  //    var rx4 = Rx(name = "rx4") { rx1() * 10 }
+  //    var rx5 = Rx(name = "rx5") { rx3() + 1 }
+  //    var rx6 = Rx(name = "rx6") { rx5() + 1 }
+  //    var rx7 = Rx(name = "rx7") { rx2() + rx4() + rx6() }
+  //
+  //    def now(rx: Rx.Types.RX): String = Option(rx) map (_.now.toString) getOrElse "N/A"
+  //    def printAll = println(Seq(now(vr1), now(vr2), now(vr3), "|", now(rx1), "|", now(rx2), now(rx3), now(rx4), "|", now(rx5), "|", now(rx6), "|", now(rx7)).mkStr)
+  //    def showGraph(vars: Rx.Types.RX*) = for ((level, rxs) <- Rx.rxsPerLevel(vars)) println(level + " : " + rxs.map(_.name).mkStr)
+  //
+  //    showGraph(vr1, vr2, vr3)
+  //
+  //    printAll
+  //    vr2 := true
+  //    printAll
+  //    vr1 := 1000
+  //    printAll
+  //    locally {
+  //      rx7 = null
+  //      rx2 = null
+  //      System.gc
+  //      printAll
   //    }
+  //    vr1 := 10000
+  //    printAll
   //  }
 
 }
